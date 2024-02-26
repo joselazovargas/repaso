c9de157126cae28bba4f54a4fd92e5556a953d63
Javier Constain <constainopia@gmail.com>
2024-02-26 22:55:22 +0100

    Add size

--- main.ts
+++ main.ts
@@ -6,7 +6,12 @@ function nameClothColor(cloth: string, color: string) {
   console.log("Here is a", color, cloth);
 }
 
+function nameClothSize(cloth: string, size: string) {
+  console.log("Here is a", cloth, "size", size);
+}
+
 (function main() {
   nameClothes("Shirt");
   nameClothColor("Jean", "blue");
+  nameClothSize("Short", "M");
 })();
