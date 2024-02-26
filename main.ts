f1adf3355150987a0a0c1e5311956d3a9b197a99
Javier Constain <constainopia@gmail.com>
2024-02-26 22:53:56 +0100

    Prints color

--- main.ts
+++ main.ts
@@ -2,6 +2,11 @@ function nameClothes(cloth: string) {
   console.log("Here is a:", cloth);
 }
 
+function nameClothColor(cloth: string, color: string) {
+  console.log("Here is a", color, cloth);
+}
+
 (function main() {
   nameClothes("Shirt");
+  nameClothColor("Jean", "blue");
 })();
