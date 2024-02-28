ac0180f296cdc456d918371b6ad6fdefd9982d34
Javier Constain <constainopia@gmail.com>
2024-02-28 21:28:37 +0100

    Add room storage (#6)

--- main.ts
+++ main.ts
@@ -2,6 +2,11 @@ function nameClothes(cloth: string) {
   console.log("Here is a:", cloth);
 }
 
+function nameClothStore(cloth: string, storage: string) {
+  console.log("The", cloth, "can be found in the", storage);
+}
+
 (function main() {
   nameClothes("Shirt");
+  nameClothStore("Jacket", "Wardrobe");
 })();
