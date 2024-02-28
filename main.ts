cf7593dfdada7d72800941a2989e42778a887e91
Jose Lazo <jose.lazo.vargas@gmail.com>
2024-02-29 06:15:26 +1000

    Revert "Add color and size to cloth" (#4)
    * Revert "Add size"
    
    This reverts commit c9de157126cae28bba4f54a4fd92e5556a953d63.
    
    * Revert "Prints color"
    
    This reverts commit f1adf3355150987a0a0c1e5311956d3a9b197a99.

--- main.ts
+++ main.ts
@@ -2,16 +2,6 @@ function nameClothes(cloth: string) {
   console.log("Here is a:", cloth);
 }
 
-function nameClothColor(cloth: string, color: string) {
-  console.log("Here is a", color, cloth);
-}
-
-function nameClothSize(cloth: string, size: string) {
-  console.log("Here is a", cloth, "size", size);
-}
-
 (function main() {
   nameClothes("Shirt");
-  nameClothColor("Jean", "blue");
-  nameClothSize("Short", "M");
 })();
