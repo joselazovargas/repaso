655869ad9351e93c6c2e4d1fa0102617b4e0c33c
Jose Lazo <jose.lazo.vargas@gmail.com>
2024-02-29 06:59:04 +1000

    ADD Status function. (#5)
    * ADD Status function.
    
    * ADD functions call.

--- main.ts
+++ main.ts
@@ -1,12 +1,22 @@
 function nameClothes(cloth: string) {
-  console.log("Here is a:", cloth);
+	console.log("Here is a:", cloth);
+}
+
+function nameClothColor(cloth: string, color: string) {
+	console.log("Here is a", color, cloth);
+}
+
+function nameClothSize(cloth: string, size: string) {
+	console.log("Here is a", cloth, "size", size);
 }
 
 function nameClothStore(cloth: string, storage: string) {
-  console.log("The", cloth, "can be found in the", storage);
+	console.log("The", cloth, "can be found in the", storage);
 }
 
 (function main() {
-  nameClothes("Shirt");
-  nameClothStore("Jacket", "Wardrobe");
+	nameClothes("Shirt");
+	nameClothColor("Jean", "blue");
+	nameClothSize("Short", "M");
+	nameClothStore("Jacket", "Wardrobe");
 })();
