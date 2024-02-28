2e7ed6eceeccad56b2c162c0251cb35fb6902008
Jose Lazo <jose.lazo.vargas@gmail.com>
2024-02-29 06:14:51 +1000

    Revert "Prints color"
    This reverts commit f1adf3355150987a0a0c1e5311956d3a9b197a99.

--- main.ts
+++ main.ts
@@ -2,11 +2,6 @@ function nameClothes(cloth: string) {
   console.log("Here is a:", cloth);
 }
 
-function nameClothColor(cloth: string, color: string) {
-  console.log("Here is a", color, cloth);
-}
-
 (function main() {
   nameClothes("Shirt");
-  nameClothColor("Jean", "blue");
 })();
