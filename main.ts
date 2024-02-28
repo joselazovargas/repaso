5e48a5681e5383b1c16ae205fce74270307f83fc
Jose Lazo <jose.lazo.vargas@gmail.com>
2024-02-29 06:22:44 +1000

    ADD Status function.

--- main.ts
+++ main.ts
@@ -1,7 +1,17 @@
 function nameClothes(cloth: string) {
-  console.log("Here is a:", cloth);
+	console.log("Here is a:", cloth);
+}
+
+function nameClothColor(cloth: string, color: string) {
+	console.log("Here is a", color, cloth);
+}
+
+function nameClothSize(cloth: string, size: string) {
+	console.log("Here is a", cloth, "size", size);
 }
 
 (function main() {
-  nameClothes("Shirt");
+	nameClothes("Shirt");
+	nameClothColor("Jean", "blue");
+	nameClothSize("Short", "M");
 })();
